c5672554b48883c2eb1ffa2b729b15f0ce71a489
CETX <zbb731@gmail.com>
2017-04-16 15:23:14 +0800

    Update reass.R

--- reass.R
+++ reass.R
@@ -5,6 +5,8 @@ library(tcltk)
 library(fitdistrplus)
 library(gdata)
 
+source("function.R")
+
 # data #
 data(fremotor1freq)
 data(fremotor1sev)
@@ -19,40 +21,6 @@ prem=fremotor1prem;
 ### data ###
 sev03=subset(sev, format(sev$OccurDate,'%Y')==2003)
 prem03=subset(prem, prem$Year==2003)
-
-### calibration ###
-Prep_Calib=function(premium){
-  prem_agg=aggregate(premium[,c("PremTot","PremWindscreen","PremDamAll","PremFire","PremAcc1","PremAcc2",
-                                "PremLegal","PremTPLM","PremTPLV","PremServ","PremTheft")], 
-                     by=list(IDpol=premium$IDpol), FUN=sum)
-  prem_agg$PremDamage=prem_agg$PremDamAll+prem_agg$PremAcc1+prem_agg$PremAcc2
-  prem_agg$PremTPL=prem_agg$PremTPLM+prem_agg$PremTPLV
-  prem_agg$PremOther=prem_agg$PremLegal+prem_agg$PremServ
-  tmp=cbind(prem_agg[,c("IDpol","PremTot","PremWindscreen","PremDamage","PremFire",
-                            "PremTPL","PremTheft","PremOther")], 
-             aggregate(prem_agg[,c("PremWindscreen","PremDamage","PremFire",
-                                   "PremTPL","PremTheft","PremOther")], 
-                       by=list(Category=prem_agg$IDpol), FUN=sign)[,-1])
-  colnames(tmp)[9:14]=c("Windscreen","Damage","Fire","TPL","Theft","Other")
-  tmp
-}
-
-Nb_sinistre=function(Severity){
-  Nb_sev=sqldf("select distinct IDpol as IDpol, count(IDpol) as nb_sinistre, 
-               Guarantee as Guarantee from Severity group by Idpol,Guarantee")
-  Nb_sev
-}
-
-# for a certain guarantee
-Prep_Calib_Freq=function(Nb_sinistre, Premium, Factor){
-  sub_severity=subset(Nb_sinistre, Guarantee==Factor)[,-3]
-  prem=Premium[,startsWith(colnames(Premium), Factor)]
-  ind = prem > 0
-  prem=Premium[ind,]
-  tmp=merge(prem, sub_severity, by="IDpol", all.x=TRUE)
-  tmp$nb_sinistre=ifelse(is.na(tmp$nb_sinistre),0,tmp$nb_sinistre)
-  tmp
-}
 
 # Calibration Frequency: By Guarantee
 Nb_Guarantee=Nb_sinistre(sev03)
@@ -70,21 +38,6 @@ Est_Fire=calib_Sev(sev03,"Fire",point_ajust=6,size=10)
 Est_TPL=calib_Sev(sev03,"TPL",point_ajust=5)
 Est_Theft=calib_Sev(sev03,"Theft",point_ajust=5)
 Est_Other=calib_Sev(sev03,"Other",point_ajust=5,ysup=1)
-
-# function for calibration of the distribution of payments by guarantee
-calib_Sev=function(Severity,Factor,point_ajust=0,size=30,ysup=0.8){
-  x=Severity[which(Severity$Guarantee==Factor),"Payment"]
-  x_log=log(x)
-  f1 <- fitdist(x_log, "gamma", method="mle")
-  f2 <- fitdist(x_log[which(x_log>=point_ajust)], "gamma", method="mle")
-  pdf (file=paste("D:/ENSAE2016-2017/projet_R_images/",Factor,".pdf"), width = 6, height = 4, paper = "special") 
-  hist(x_log,size,ylim=c(0,ysup),freq=FALSE,main=paste("Fitting of log_severity for guarantee",Factor))
-  curve(dgamma(x, shape=f1$estimate[1], rate = f1$estimate[2]), add=TRUE,col = "blue")
-  curve(dgamma(x, shape=f2$estimate[1], rate = f2$estimate[2]), add=TRUE,col = "red")
-  dev.off()
-  return(f2$estimate)
-}
-
 
 
 ### genarator ###
